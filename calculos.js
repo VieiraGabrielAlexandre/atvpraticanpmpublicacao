d879633f2e4321875cd8bbe4d18c23b48d20b6bc
Gabriel Vieira <gabriel.vieira@origoenergia.com.br>
2023-05-30 16:37:02 -0300

    first

---
+++ calculos.js
@@ -0,0 +1,26 @@
+function adicao(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    if (b === 0) {
+        return "absurdo matematico";
+    }
+
+    return a / b;
+}
+
+module.exports = {
+    adicao,
+    subtracao,
+    multiplicacao,
+    divisao
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+const calculos = require('./calculos.js');
+
+console.log("a) 1 + 1 = " + calculos.adicao(1, 1))
+console.log("b) -123 + 123123 = " + calculos.adicao(-123, 123123))
+console.log("a) 8 * 0 = " + calculos.multiplicacao(8, 0))
+console.log("a) 1239123 * 12313 = " + calculos.adicao(1239123, 12313))
+console.log("a) 123 / -12 = " + calculos.divisao(123, -12))
+console.log("a) 313123 / 0 = " + calculos.divisao(313123, 0))
